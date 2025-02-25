1c71b79bcb2980bd67edcac83c49b79defc1d622
EC2 Default User <ec2-user@ip-172-31-28-121.ap-northeast-1.compute.internal>
2025-02-25 11:16:10 +0000

    コミット忘れ

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,54 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+    @lists = List.all
+  end
+  
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      flash[:nitice] = "投稿完了しました。"
+    redirect_to list_path(@list.id)
+    else
+      @lists = List.all
+      render :index
+    end
+  end
+
+  def index
+    @lists = List.all
+    @list  =List.new
+  end
+
+  def show
+    @list = List.find(params[:id]) 
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      flash[:notice] = "編集完了しました。"
+    redirect_to list_path(@list.id)  
+    else
+      @lists = List.all
+      render :edit
+    end
+  end
+  
+  def destroy
+    list = List.find(params[:id])  
+    list.destroy  
+    redirect_to '/lists'  
+    flash[:notice] = "削除完了しました。"
+  end
+  
+  private 
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'new/index'
+  get 'new/show'
+  devise_for :users
+  
+  get '/homes/about' => 'homes#about',as:'about'
+  get 'lists/new'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as:'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  resources :users, only: [:show, :edit]
+  resources :post_images, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
